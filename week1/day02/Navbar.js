bdb9195bf325e0e1e1c5a9538bf253cf50d1ac1a
Nithya232005 <nithyathangaraju23@gmail.com>
2025-12-23 15:08:37 +0530

    Add files via upload

---
+++ week1/day02/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">MyProfile</h2>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
